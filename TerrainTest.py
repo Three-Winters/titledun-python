67d99e353a874c6214fa1ebf70b92964d0127404
vilivi <24197641+vilivi@users.noreply.github.com>
2019-08-20 01:51:18 -0400

    Add to globals class, add move function to TerrainTest

--- TerrainTest.py
+++ TerrainTest.py
@@ -5,3 +5,35 @@ class TerrainTest():
 	def __init__(self):
 		print("TerrainTest object created")
 		self.tm = TerrainManager()
+		Globals.g_task_manager.add(self.move, "moveTask")
+
+	#test this below
+	def __del__(self):
+		Globals.g_task_manager.remove("moveTask")
+
+	def move(self, task):
+
+		#dt is the time since last frame
+		dt = globalClock.get_dt()
+
+		if Globals.key_map["left"]:
+			Globals.camera.setX(Globals.camera.getX() - 5.0)
+		if Globals.key_map["right"]:
+			Globals.camera.setX(Globals.camera.getX() + 5.0)
+		if Globals.key_map["forward"]:
+			Globals.camera.setZ(Globals.camera.getZ() + 5.0)
+		if Globals.key_map["backward"]:
+			Globals.camera.setZ(Globals.camera.getZ() - 5.0)
+		if Globals.key_map["mouse3"]:
+			c_x_pos = Globals.win.getPointer(0).getX()
+			c_y_pos = Globals.win.getPointer(0).getY()
+			c_x_pos -= self.r_x_pos
+			c_y_pos -= self.r_y_pos
+			Globals.controls.alpha -= c_x_pos * 0.05
+			Globals.controls.beta -= c_y_pos * 0.05
+			Globals.camera.setHpr(Globals.controls.alpha, Globals.controls.beta, 0.0)
+			Globals.win.movePointer(0, int(self.r_x_pos), int(self.r_y_pos))
+		if not Globals.key_map["mouse3"]:
+			self.r_x_pos = Globals.win.getPointer(0).getX()
+			self.r_y_pos = Globals.win.getPointer(0).getY()
+		return task.cont
--- main.py
+++ main.py
@@ -27,38 +27,14 @@ class MyApp(ShowBase):
 		self.win.request_properties(props)
 		self.win.set_clear_color((0, 0, 0, 1))
 
+
 		Globals.game_states.request("MainMenu")
 
-		taskMgr.add(self.move, "moveTask")
+		#taskMgr.add(self.move, "moveTask")
 		Globals.g_task_manager = taskMgr
-		self.cons = Controls()
-
-	def move(self, task):
-
-		#dt is the time since last frame
-		dt = globalClock.get_dt()
-
-		if Globals.key_map["left"]:
-			self.camera.setX(self.camera.getX() - 5.0)
-		if Globals.key_map["right"]:
-			self.camera.setX(self.camera.getX() + 5.0)
-		if Globals.key_map["forward"]:
-			self.camera.setZ(self.camera.getZ() + 5.0)
-		if Globals.key_map["backward"]:
-			self.camera.setZ(self.camera.getZ() - 5.0)
-		if Globals.key_map["mouse3"]:
-			c_x_pos = self.win.getPointer(0).getX()
-			c_y_pos = self.win.getPointer(0).getY()
-			c_x_pos -= self.r_x_pos
-			c_y_pos -= self.r_y_pos
-			self.cons.alpha -= c_x_pos * 0.05
-			self.cons.beta -= c_y_pos * 0.05
-			self.camera.setHpr(self.cons.alpha, self.cons.beta, 0.0)
-			self.win.movePointer(0, int(self.r_x_pos), int(self.r_y_pos))
-		if not Globals.key_map["mouse3"]:
-			self.r_x_pos = self.win.getPointer(0).getX()
-			self.r_y_pos = self.win.getPointer(0).getY()
-		return task.cont
+		Globals.win = self.win
+		Globals.controls = Controls()
+		Globals.camera = self.camera
 
 app = MyApp()
 app.run()
