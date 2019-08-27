dc58b409b1151fe031b52f61a6fab59156e855d8
vilivi <24197641+vilivi@users.noreply.github.com>
2019-08-27 00:37:03 -0400

    Fixed rotation issue with movement, related to node inheritance

--- TerrainTest.py
+++ TerrainTest.py
@@ -17,11 +17,15 @@ class TerrainTest():
 		Globals.g_task_manager.remove("moveTask")
 
 	def make_actor(self):
+		self.pc_node = base.render.attachNewNode("pcnode")
 		self.pc = Actor("models/panda-model", {"walk": "models/panda-walk4"})
-		self.pc.setScale(0.005, 0.005, 0.005)
-		self.pc.reparentTo(base.render)
+		self.pc.setScale(0.05, 0.05, 0.05)
+		self.pc_node.reparentTo(self.pc)
 		self.pc.setPos(0,0,0)
-		base.camera.reparentTo(self.pc)
+		self.pc_node.setCompass()
+		self.pc.reparentTo(base.render)
+		base.camera.reparentTo(self.pc_node)
+		base.camLens.setFar(500000000000.0)
 		base.camera.setPos(0, 0, 0)
 
 	def move(self, task):
@@ -29,22 +33,52 @@ class TerrainTest():
 		#dt is the time since last frame
 		dt = globalClock.get_dt()
 
+		speed = 5.0
+		new_x = self.pc.getX()
+		new_y = self.pc.getY()
+		new_z = self.pc.getZ()
+		new_head = self.pc.getH()
+
+		if not Globals.key_map["mouse3"]:
+			self.r_x_pos = base.win.getPointer(0).getX()
+			self.r_y_pos = base.win.getPointer(0).getY()
+
+
 		if Globals.key_map["left"]:
-			base.camera.setX(self.pc.getX() - 5.0)
+			new_x -= cos(Globals.DEG_TO_RAD*Globals.controls.alpha)*speed
+			new_y -= sin(Globals.DEG_TO_RAD*Globals.controls.alpha)*speed
+			new_head = Globals.controls.alpha-90
+			if Globals.key_map["forward"]: #both left and foward held
+				new_head = Globals.controls.alpha-135
+			elif Globals.key_map["backward"]:
+				new_head = Globals.controls.alpha-45
 		if Globals.key_map["right"]:
-			base.camera.setX(self.pc.getX() + 5.0)
+			new_x += cos(Globals.DEG_TO_RAD*Globals.controls.alpha)*speed
+			new_y += sin(Globals.DEG_TO_RAD*Globals.controls.alpha)*speed
+			if Globals.key_map["forward"]:
+				new_head = Globals.controls.alpha+135
+			elif Globals.key_map["backward"]:
+				new_head = Globals.controls.alpha+45
+			else:
+				new_head = Globals.controls.alpha+90
 		if Globals.key_map["forward"]:
-			base.camera.setZ(self.pc.getZ() + 5.0)
+			new_x -= sin(Globals.DEG_TO_RAD*Globals.controls.alpha)*speed
+			new_y += cos(Globals.DEG_TO_RAD*Globals.controls.alpha)*speed
+			if not Globals.key_map["right"] and not Globals.key_map["left"]:
+				new_head = Globals.controls.alpha+180
 		if Globals.key_map["backward"]:
-			base.camera.setZ(self.pc.getZ() - 5.0)
+			new_x += sin(Globals.DEG_TO_RAD*Globals.controls.alpha)*speed
+			new_y -= cos(Globals.DEG_TO_RAD*Globals.controls.alpha)*speed
+			if not Globals.key_map["right"] and not Globals.key_map["left"]:
+				new_head = Globals.controls.alpha
 
 		if Globals.key_map["mouse3"]:
 			c_x_pos = base.win.getPointer(0).getX()
 			c_y_pos = base.win.getPointer(0).getY()
 			c_x_pos -= self.r_x_pos
 			c_y_pos -= self.r_y_pos
-			Globals.controls.alpha -= c_x_pos * 0.05
-			Globals.controls.beta -= c_y_pos * 0.05
+			Globals.controls.alpha -= c_x_pos*0.05
+			Globals.controls.beta -= c_y_pos*0.05
 			base.camera.setHpr(Globals.controls.alpha, Globals.controls.beta, 0.0)
 			base.win.movePointer(0, int(self.r_x_pos), int(self.r_y_pos))
 
@@ -55,7 +89,9 @@ class TerrainTest():
 								   cos(Globals.controls.alpha*Globals.DEG_TO_RAD)*-(abs(camera_dist)),
 								   cos(Globals.controls.beta*Globals.DEG_TO_RAD)*camera_z_dist)
 
-		if not Globals.key_map["mouse3"]:
-			self.r_x_pos = base.win.getPointer(0).getX()
-			self.r_y_pos = base.win.getPointer(0).getY()
+
+		#Set the new position and rotation
+		self.pc.setPos(new_x, new_y, new_z)
+		#self.pc_node.setHpr(new_head, 0, 0)
+		self.pc.setHpr(new_head, Globals.controls.beta, 0.0)
 		return task.cont
